f88b5a0a1566debc6b59f7f76e11920d56efb27f
drodata <drodata@foxmail.com>
2014-12-13 17:24:03 +0800

    syn

--- protected/views/demo/on.php
+++ protected/views/demo/on.php
@@ -1,6 +1,19 @@
 <button class="a btn btn-primary">Go</button>
 <button id="b" class="btn btn-primary">Copy</button>
 <hr>
+<form method="post">
+	<input type="text" name="name" />
+	<label><input type="radio" value="a" name="food" />Food A</label>
+	<label><input type="radio" value="b" name="food" />Food B</label>
+
+	<button type="submit">Go</button>
+</form>
+<?php
+	if ($_POST) {
+		K::printR($_POST);
+	}
+?>
+<hr>
 <div id="paste">
 </div>
 <div id="console"></div>
