d84feac61d2f2a4c8708107d3d1d96dacacd2557
drodata <drodata@foxmail.com>
2015-07-18 16:12:26 +0800

    Failed to practice web service #13
    XML declaration allowed only at the start of the document

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $yii=dirname(__FILE__).'/../api/yii/framework-16/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
- defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 
